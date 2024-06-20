1e513b0a92115df72c761806b647ea4740c48428
4-I-KaedeYuto <g02032@ktc.ac.jp>
2024-06-20 14:58:35 +0900

    Homeにメニュー表示ボタンを追加

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
 'use client'
 
-import styles from "./page.module.scss";
+import { useContext } from "react";
+import { ContextShowMenu } from "./layout";
+import { Button } from "react-bootstrap";
+
 export default function Home() {
+  const { setShowMenu } = useContext(ContextShowMenu);
   return (
       // <App />
-      <></>
+      <>
+        <Button variant="primary" onClick={() => setShowMenu(true)} >メニューを表示</Button>
+      </>
   );
 }

44ad0072c81e6e259e2eea473d677e93f5826591
Sata-Kyota <g02057@ktc.ac.jp>
2024-10-24 11:55:23 +0900

    offcanvasのhrefを相対パスに変更

--- components/top/MyOffcanvas.js
+++ components/top/MyOffcanvas.js
@@ -17,12 +17,12 @@ export default function MyOffcanvas() {
           </Offcanvas.Header>
           <Offcanvas.Body>
             <ListGroup>
-              <ListGroup.Item><a href="/">Home</a></ListGroup.Item>
-              <ListGroup.Item><a href="/projectile/freefall">自由落下</a></ListGroup.Item>
-              <ListGroup.Item><a href="/projectile/throwup">鉛直投げ上げ</a></ListGroup.Item>
-              <ListGroup.Item><a href="/projectile/horizontal">水平投射</a></ListGroup.Item>
-              <ListGroup.Item><a href="/projectile/oblique">斜方投射</a></ListGroup.Item>
-              <ListGroup.Item><a href="/projectile/monkeyhunting">モンキーハンティング</a></ListGroup.Item>
+              <ListGroup.Item><a href="./">Home</a></ListGroup.Item>
+              <ListGroup.Item><a href="./projectile/freefall">自由落下</a></ListGroup.Item>
+              <ListGroup.Item><a href="./projectile/throwup">鉛直投げ上げ</a></ListGroup.Item>
+              <ListGroup.Item><a href="./projectile/horizontal">水平投射</a></ListGroup.Item>
+              <ListGroup.Item><a href="./projectile/oblique">斜方投射</a></ListGroup.Item>
+              <ListGroup.Item><a href="./projectile/monkeyhunting">モンキーハンティング</a></ListGroup.Item>
             </ListGroup>
           </Offcanvas.Body>
         </Offcanvas>
